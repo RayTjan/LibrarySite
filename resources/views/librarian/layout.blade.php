d1868faa036d72224b9c96411ce80b622bc9d64a
RayTjan <email4tjandra@gmail.com>
2022-12-17 06:37:20 +0700

    before trying Laravel Breeze

--- resources/views/librarian/layout.blade.php
+++ resources/views/librarian/layout.blade.php
@@ -3,9 +3,15 @@
 <head>
     <title>Librarian site</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ '/css/customStyle.css' }}" rel="stylesheet" type="text/css" >
 </head>
 <body>
-  
+  <div>hello</div>
 <div class="container">
     @yield('content')
 </div>
