5abd7354d1c19c9b3ffcd50a0c57c49a457246b1
RayTjan <email4tjandra@gmail.com>
2022-12-17 06:59:37 +0700

    NavBar & Authentication
    Used Laravel Breeze and made it my own, somewhat

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LibrarianController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +16,19 @@ use App\Http\Controllers\LibrarianController;
 */
 
 Route::get('/', function () {
-    return view('librarian.index');
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::resource('librarian', LibrarianController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
