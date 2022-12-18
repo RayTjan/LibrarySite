08afcd8ea0b67a4c077fd80102b1cb34cf349224
RayTjan <email4tjandra@gmail.com>
2022-12-18 23:20:17 +0700

    leftout update

--- resources/views/librarian/index.blade.php
+++ resources/views/librarian/index.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Welcome to the Library</h2>
+                <h2>Book Catalog</h2>
             </div>
             <div class="pull-right">
                 <a class="btn bg-maincolor text-white" href="{{ route('book.create') }}"> Add New Book</a>
